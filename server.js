85d2c6ddeca0c0514248cfa8962bed043c6f575b
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2024-11-17 15:24:54 +0100

    heelo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Chargement du fichier db.json
-const produits = require('./db.json');
+const produits = require('db.json');
 
 // Définir un point d'API pour récupérer les produits
 app.get('/api/produits', (req, res) => {
